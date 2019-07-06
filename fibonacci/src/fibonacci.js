d5a09563db2d0613550f9f6319c98dc4efb848cb
Anders Flink <andflink@gmail.com>
2019-07-06 16:47:59 +0200

    small changes, started using yarn

--- fibonacci/src/fibonacci.js
+++ fibonacci/src/fibonacci.js
@@ -1,68 +1,56 @@
 class Fibonacci {
-	encode(number) {
-        const getFib = (number) => {
-            var fib = [];
-            let index = 2;
+  static getFib(number) {
+    var fib = [];
+    let index = 2;
 
-            fib[0] = 1;
-            fib[1] = 2;
+    fib[0] = 1;
+    fib[1] = 2;
 
-            for (index; fib[index - 1] <= number; index++) {
-                fib[index] = fib[index - 1] + fib[index - 2]; 
-            }
-
-            return { fib, i: index - 2};
-        };
-
-        let codeword = [];
-        let { fib, i } = getFib(number);
-
-        while (number) {
-            codeword[i] = '1'; 
-
-            number = number - fib[i--]; 
-      
-            while (i >= 0 && fib[i] > number) { 
-                codeword[i--] = '0'; 
-            } 
-        }
-
-        codeword[fib.length + 1] = '1';
-
-        return codeword.join('');
+    for (index; fib[index - 1] <= number; index++) {
+      fib[index] = fib[index - 1] + fib[index - 2];
     }
 
-    decode(string) {
-        const getFib = (number) => {
-            var fib = [];
-            let index = 2;
+    return { fib, i: index - 2 };
+  }
 
-            fib[0] = 1;
-            fib[1] = 2;
+  encode(number) {
+    let codeword = [];
+    let { fib, i } = Fibonacci.getFib(number);
 
-            for (index; fib[index - 1] <= number; index++) {
-                fib[index] = fib[index - 1] + fib[index - 2]; 
-            }
+    while (number) {
+      codeword[i] = '1';
+      number = number - fib[i--];
 
-            return fib;
-        };
-
-        let value = 0;
-        let splitString = string.split('');
-
-        if (splitString.pop() === '1' && splitString[splitString.length - 1] === '1') {
-            let fib = getFib(splitString.length);
-            let i = 0;
-            
-            for (i; i < splitString.length; i++) {
-                if (splitString[i] === '1') {
-                    value += fib[i];
-                }
-            }
-        }
-
-        return value;
+      while (i >= 0 && fib[i] > number) {
+        codeword[i--] = '0';
+      }
     }
+
+    codeword[fib.length + 1] = '1';
+
+    return codeword.join('');
+  }
+
+  decode(string) {
+    let value = 0;
+    let splitString = string.split('');
+
+    if (
+      splitString.pop() === '1' &&
+      splitString[splitString.length - 1] === '1'
+    ) {
+      const { fib } = Fibonacci.getFib(splitString.length);
+      let i = 0;
+
+      for (i; i < splitString.length; i++) {
+        if (splitString[i] === '1') {
+          value += fib[i];
+        }
+      }
+    }
+
+    return value;
+  }
 }
 
 module.exports = new Fibonacci();
